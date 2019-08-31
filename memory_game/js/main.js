ba0ca926902021926975f5cdec16c4ffc0513c84
2easy2do <enriquealexy@gmail.com>
2019-08-30 20:54:17 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,4 @@
-var cards = [
-  {
+var cards = [{
     rank: "queen",
     suit: "hearts",
     cardImage: "images/queen-of-hearts.png"
@@ -22,6 +21,15 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('img', 'images/back-png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
 
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -31,18 +39,13 @@ function checkForMatch() {
   }
 }
 
-function flipCard(cardId) {
-  console.log("User flipped " + cards[cardId].rank);
-
+function flipCard() {
+  var cardId = this.getAttribute('data-id');
+  this.setAttribute('src', cards[cardId].cardimage)
   cardsInPlay.push(cards[cardId].rank);
-
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
-
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
